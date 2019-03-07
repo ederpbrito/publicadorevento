7de191f2f73247b74a6395b9b34c6fd68ee7f14b
Eder Brito <ederpsilva@hotmail.com>
2019-03-07 20:45:29 -0300

    first commit

---
+++ src/main/java/br/com/smartfinance/PublicadorEventoApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicadorEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicadorEventoApplication.class, args);
+	}
+
+}
